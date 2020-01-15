c8d364aa2d1ed26eb607d57db9aa6915c53d3a76
Milan Parmar <milan_parmar@hotmail.co.uk>
2020-01-15 17:14:32 +0000

    Automatically backed up by Learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -13,4 +13,17 @@ class EmailAddressParser
     @@email << self 
   end 
   
+  rows = csv_data.split("\n")
+people = rows.collect do |row|
+  data = row.split(", ")
+  name = data[0]
+  age = data[1]
+  company = data[2]
+  person = Person.new
+  person.name = name
+  person.age = age
+  person.company = company
+  person
+end
+  
 end 
